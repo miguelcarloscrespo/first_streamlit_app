69889b470f1825fb91cf659ff5e426889ea835f3
Miguel Crespo <121422904+miguelcarloscrespo@users.noreply.github.com>
2022-12-26 21:31:33 +1300

    Update streamlit_app.py
    Two menues

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
+streamlit.title('My breakkie')
 streamlit.text('Scrambled eggs')
 streamlit.text('Avocado & Cheese')
 streamlit.text('Porridge')
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
